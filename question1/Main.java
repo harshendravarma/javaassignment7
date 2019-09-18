b79cace42040b9760039a1b86eef122b7f0089cd
harshendravarma <47440162+harshendravarma@users.noreply.github.com>
2019-09-18 12:44:45 +0530

    added comments

--- question1/Main.java
+++ question1/Main.java
@@ -2,18 +2,20 @@ package question1;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		Rodent r[] = new Rodent[3];
-		r[0] = new Mouse();
-		r[1] = new Gerbil();
-		r[2] = new Hamster();
+		
+		r[0] = new Mouse();		//output: I am a Mouse derived from rodent,I am a Rodent
+		r[1] = new Gerbil();	//output: I am a Gerbil derived from rodent,I am a Rodent
+		r[2] = new Hamster();	//output: I am a Hamster derived from rodent,Mouse is eating
 
-		r[0].eat();
-		r[0].run();
+		r[0].eat();		//output: Mouse is eating
+		r[0].run();		//output: Mouse is eating
 
-		r[1].eat();
-		r[1].run();
+		r[1].eat();		//output: Gerbil is eating
+		r[1].run();		//output: Gerbil is running
 
-		r[2].eat();
-		r[2].run();
+		r[2].eat();		//output: Hamster is eating
+		r[2].run();		//output: Hamster is running
 	}
 }
